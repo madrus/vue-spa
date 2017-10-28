39876b4a7a6dd4537b2c99ec45279ee4a967156d
Andre Roussakoff <madrus@gmail.com>
2017-10-28 16:43:04 +0200

    used child components

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,3 +1,10 @@
 <template>
-
+  <nav class="nav has-shadow">
+    <div class="container">
+      <a href="/">
+        <img src="http://bstavroulakis.com/pluralsight/courses/vue-spa/logo.png"
+          alt="Vue SPA" />
+      </a>
+    </div>
+  </nav>
 </template>
